7fddeab4a00036713693e98d02ec5cb3e663ade7
audricsan <audricrosier@gmail.com>
2026-07-09 23:42:15 +0900

    chore(release): v0.5.0

--- config/app.php
+++ config/app.php
@@ -8,5 +8,5 @@ return [
     'timezone' => env('APP_TIMEZONE', 'Asia/Tokyo'),
     'locale' => env('APP_LOCALE', 'ja'),
     'url' => env('APP_URL', ''),
-    'version' => env('APP_VERSION', '0.0.3-beta'),
+    'version' => env('APP_VERSION', '0.5.0'),
 ];
